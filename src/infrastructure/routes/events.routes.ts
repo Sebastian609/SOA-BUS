c92b8b0e7a84e6c0f62303f0d94ce50ce92c04fd
Sebastian609 <62573264+Sebastian609@users.noreply.github.com>
2025-07-19 17:28:30 -0500

    Remove authentication middleware from events routes to streamline proxy handling.

--- src/infrastructure/routes/events.routes.ts
+++ src/infrastructure/routes/events.routes.ts
@@ -4,7 +4,7 @@ import proxy from "express-http-proxy";
 import { authenticateToken } from "../middleware/authJwt";
 import { autenticatePartner } from "../middleware/authPartnerToken";
 import { config } from "dotenv";
-config()
+config();
 
 export class EventsRoutes {
   public router: Router;
@@ -15,29 +15,26 @@ export class EventsRoutes {
   }
 
   private routes() {
-
-   this.router.use(
-  "/paginated",
-  autenticatePartner,
-  proxy(`${process.env.EVENT_SERVICE_URL}`, {
-    proxyReqPathResolver: req => req.originalUrl, // 👈 evita duplicar el path
-    userResDecorator: async (proxyRes, proxyResData, req, res) => {
-      try {
-        return JSON.parse(proxyResData.toString("utf8"));
-      } catch (err) {
-        return proxyResData.toString("utf8");
-      }
-    }
-  })
-);
-
+    this.router.use(
+      "/paginated",
+      proxy(`${process.env.EVENT_SERVICE_URL}`, {
+        proxyReqPathResolver: (req) => req.originalUrl, // 👈 evita duplicar el path
+        userResDecorator: async (proxyRes, proxyResData, req, res) => {
+          try {
+            return JSON.parse(proxyResData.toString("utf8"));
+          } catch (err) {
+            return proxyResData.toString("utf8");
+          }
+        },
+      })
+    );
 
     // 🔒 Todas las demás rutas: protegidas
     this.router.use(
       "/",
       authenticateToken,
       proxy(process.env.EVENT_SERVICE_URL, {
-        proxyReqPathResolver: req => req.originalUrl
+        proxyReqPathResolver: (req) => req.originalUrl,
       })
     );
   }
